63f19022abd9a1e652cb85cc6e30054146a38158
android <fouche@et.esiea.fr>
2025-05-14 09:02:17 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-student-city',
+  webDir: 'www'
+};
+
+export default config;
